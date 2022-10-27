b495c240c61156f6e9bfa79d701d1f56e66d98d5
Andrea Hohnsen <a.hohnsen@mail.de>
2022-10-27 10:36:07 +0200

    Initial commit

---
+++ components/GlobalStyles.js
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+* {
+  box-sizing: border-box;
+}
+
+body {
+  padding: 20px;
+  background-color: seagreen;
+  color: white;
+}
+`;
+
+export default GlobalStyle;
---
+++ pages/veggie/index.js
@@ -0,0 +1,12 @@
+export default function Veggie() {
+  return (
+    <>
+      <h1>Veggie</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem voluptatum tempore
+        aliquam eius, atque repudiandae nesciunt necessitatibus aperiam. Commodi dolor optio facilis
+        minus veniam cupiditate corporis iste error explicabo iusto.
+      </p>
+    </>
+  );
+}
