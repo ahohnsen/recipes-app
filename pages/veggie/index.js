31d59af833c466b465945b0eca0aa056f6515851
Andrea Hohnsen <a.hohnsen@mail.de>
2022-10-27 13:15:32 +0200

    Add Image from public

--- pages/veggie/index.js
+++ pages/veggie/index.js
@@ -1,7 +1,16 @@
+import Image from "next/image";
+import pizzaPicture from "../../public/images/pizza.jpg";
+
 export default function Veggie() {
   return (
     <>
       <h2>Veggie</h2>
+      <Image
+        src={pizzaPicture}
+        alt="Picture of a veggie pizza"
+        layout="responsive"
+        width="400"
+      ></Image>
       <p>
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem voluptatum tempore
         aliquam eius, atque repudiandae nesciunt necessitatibus aperiam. Commodi dolor optio facilis
